de5915167c1ea117ed7eb954f68b00092da07c4a
Ishpreet Rattan <ishpreet_rattan@hotmail.com>
2015-01-20 16:37:11 -0800

    created Guess.php with method index Signed-off-by:Ishpreet Rattan <ishpreet_rattan@hotmail.com>

---
+++ application/controllers/Guess.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of guess
+ *
+ * @author Administrator
+ */
+class Guess extends Application {
+    
+    function index() {
+        $this->data['pagebody'] = 'justone';
+        $source = $this->quotes->get(4);
+        $who = $source['who'];
+        $what = $source['what'];
+        $mug = $source['mug'];
+        $this->data['who'] = $who;
+        $this->data['what'] = $what;
+        $this->data['mug'] = $mug;
+        $this->render();
+     }
+}
